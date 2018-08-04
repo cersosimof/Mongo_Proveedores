a1749b0054b99e24377ae439809b38056a4df64b
Facundo Cersosimo <cherso@Facundos-MacBook-Pro.local>
2018-08-04 18:07:57 -0300

    Arreglo de bases de datos

--- public/models/modelUsuario.js
+++ public/models/modelUsuario.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // mongoose.connect("mongodb://localhost/proveedores");
-mongoose.connect("mongodb://<cersosimof>:<espora1436>@ds263791.mlab.com:63791/prov_crud", { useNewUrlParser: true });
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds113522.mlab.com:13522/proveedores');
+// mongodb://<dbuser>:<dbpassword>@ds263791.mlab.com:63791/prov_crud
 
 var user_schema = new Schema({
     user : String,
