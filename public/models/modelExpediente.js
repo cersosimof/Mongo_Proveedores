e58054f2a92b49c88b43df625018a2ee1ff5f653
Facundo Cersosimo <cherso@Facundos-MacBook-Pro.local>
2018-08-04 17:52:03 -0300

    problema de base de datos

--- public/models/modelExpediente.js
+++ public/models/modelExpediente.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect("mongodb://localhost/proveedores");
+// mongoose.connect("mongodb://localhost/proveedores");
+mongoose.connect("mongodb://<cersosimof>:<espora1436>@ds263791.mlab.com:63791/prov_crud", { useNewUrlParser: true });
 
 var exp_schema = new Schema({
     nroExp : Number,
--- public/models/modelProveedor.js
+++ public/models/modelProveedor.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect("mongodb://localhost/proveedores");
+// mongoose.connect("mongodb://localhost/proveedores");
+mongoose.connect("mongodb://<cersosimof>:<espora1436>@ds263791.mlab.com:63791/prov_crud", { useNewUrlParser: true });
 
 var proveedor_schema = new Schema({
     nombre : String,
